4973384f7b950ec63579718f4ae308c584afec5b
root <root@77341839821c.mylabserver.com>
2021-08-03 20:45:37 +0000

    jenkins

--- jenkins/levantar-jenkins.sh
+++ jenkins/levantar-jenkins.sh
@@ -1,1 +1,1 @@
-docker run -dit -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins-curso jenkins-sc
+docker run -dit -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins-curso jenkins-it
