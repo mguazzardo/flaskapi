4da6ca35232138f62a695fb88669fba19c410792
mguazzardo <mguazzardo76@gmail.com>
2021-08-03 17:29:42 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -29,7 +29,7 @@ pipeline {
       steps {
         withCredentials([usernamePassword(credentialsId: 'ssh-key', usernameVariable: 'USUARIO', passwordVariable: 'CONTRASENIA')]) {
           sh '''
-           sshpass -p ${CONTRASENIA} ssh -o StrictHostKeyChecking=no ${USUARIO}@172.17.0.1 'kubectl create deployment flaskapi --image=mguazzardo/flaskapi -n flaskapi'
+           sshpass -p ${CONTRASENIA} ssh -o StrictHostKeyChecking=no ${USUARIO}@54.153.12.169 'kubectl create deployment flaskapi --image=mguazzardo/flaskapi -n flaskapi'
           '''
         }
         
