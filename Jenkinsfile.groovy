f322b45ef2fd190180615c37d985a9061a6ad5f8
mguazzardo <mguazzardo76@gmail.com>
2021-08-03 13:54:54 -0300

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+  environment {
+    registry = "mguazzardo/curso-jenkins"
+    registryCredential = 'dockerhub'
+  }
+ agent any
+  stages {
+
+stage('Build Docker Image') {
+            when {
+                branch 'master'
+            }
+            steps {
+                script {
+                    app = docker.build("mguazzardo/flaskapi:latest")
+                }
+            }
+        }
+
+   stage('Push Docker Image') {
+            when {
+                branch 'master'
+            }
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                        app.push("${env.BUILD_NUMBER}")
+                        app.push("latest")
+                    }
+                }
+            }
+        }
+    }   
+}
+}
+}
